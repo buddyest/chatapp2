a011d4a2fd3bd48f533116f40624bccb634d18d9
Shubham Kumar <shubhamk773@gmail.com>
2020-10-25 02:14:39 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,12 +8,11 @@ const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 const router = require('./router');
 
 const app = express();
-app.use(
-  cors({
-    origin: [http://localhost:5000','http://127.0.0.1:5000],
-    credentials: true
-  })
-);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 const server = http.createServer(app);
 const io = socketio(server);
 
