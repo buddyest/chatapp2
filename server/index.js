15df75353527e8dcb720265d98886f3df41838bd
imshubhamkumar <shubhamk773@gmail.com>
2020-10-26 02:04:32 +0530

    cors error fixed

--- server/index.js
+++ server/index.js
@@ -19,10 +19,8 @@ app.use(cors());
 app.use(express.json());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-// io.set( 'origins', '*chatstep.herokuapp.com' );
 io.origins('https://chatstep.herokuapp.com'); 
 io.on('connect', (socket) => {
   socket.on('join', ({ name, room }, callback) => {
