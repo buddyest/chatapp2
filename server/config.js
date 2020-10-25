ba58476308a5a5687266fbaabb8de5b1d4d28f92
imshubhamkumar <shubhamk773@gmail.com>
2020-10-26 01:20:34 +0530

    cors error fixed

--- server/config.js
+++ server/config.js
@@ -1,2 +1,2 @@
-//module.exports.ENDPOINT = 'https://chatstep.herokuapp.com/'
-module.exports.ENDPOINT = 'http://localhost:5000'
+module.exports.ENDPOINT = 'https://chatstep.herokuapp.com/'
+// module.exports.ENDPOINT = 'http://localhost:5000'
